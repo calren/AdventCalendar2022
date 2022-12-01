5d356ad595a3be3d8e5899e1331d271157093ea5
Caren <calren24@gmail.com>
2022-12-01 01:12:26 -0800

    Add priorityqueue implementation for day 1, part 2

--- src/main/kotlin/day1/Part2.kt
+++ src/main/kotlin/day1/Part2.kt
@@ -1,9 +1,10 @@
 package day1
 
 import java.io.File
+import java.util.*
 
 fun main(args: Array<String>) {
-    findTop3ElfCarryingMostCalories("/Users/carenc/Desktop/AdventCode2022/src/main/kotlin/day1/input.txt")
+    findTop3WithPQ("/Users/carenc/Desktop/AdventCode2022/src/main/kotlin/day1/input.txt")
 }
 
 fun findTop3ElfCarryingMostCalories(fileName: String) {
@@ -23,4 +24,25 @@ fun findTop3ElfCarryingMostCalories(fileName: String) {
     val sorted = listOfCalories.sortedDescending()
 
     print(sorted.get(0) + sorted.get(1) + sorted.get(2))
+}
+
+fun findTop3WithPQ(fileName: String) {
+    val top3: PriorityQueue<Int> = PriorityQueue<Int>(3, Collections.reverseOrder())
+
+    var currentSum = 0
+    File(fileName).forEachLine {
+        if (it.isNotBlank()) {
+            currentSum += it.toInt()
+        } else {
+            top3.add(currentSum)
+            currentSum = 0
+        }
+    }
+
+    var finalTop3Sum = 0
+    for (i in 1..3) {
+        finalTop3Sum += top3.poll()
+    }
+
+    print(finalTop3Sum)
 }
